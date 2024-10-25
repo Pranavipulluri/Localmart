cf88ef37e05aec7151fd08e24b550cc51a0432eb
Pranavipulluri <pulluripranavi@gmail.com>
2024-10-25 16:31:58 +0530

    fourth commit

---
+++ app/src/main/java/org/meicode/recycler/ProductRequestAdapter.java
@@ -0,0 +1,70 @@
+package org.meicode.recycler;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ProductRequestAdapter extends RecyclerView.Adapter<ProductRequestAdapter.ViewHolder> {
+
+    private List<ProductRequest> productRequests;
+
+    public ProductRequestAdapter(List<ProductRequest> productRequests) {
+        this.productRequests = productRequests;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product_request, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ProductRequest productRequest = productRequests.get(position);
+        holder.productDescription.setText(productRequest.getDescription());
+        holder.userDetails.setText(productRequest.getUserDetails());
+        holder.productImage.setImageResource(productRequest.getImageResourceId());
+
+        // Handle the message button click
+        holder.messageCustomerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // You can implement your messaging logic here
+                // For example, show a dialog or navigate to a chat screen
+                // For simplicity, you can show a toast or log the message
+                // Toast.makeText(v.getContext(), "Message to: " + productRequest.getUserDetails(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return productRequests.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView productImage;
+        TextView productDescription;
+        TextView userDetails;
+        Button messageCustomerButton;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            productImage = itemView.findViewById(R.id.productImage);
+            productDescription = itemView.findViewById(R.id.productDescription);
+            userDetails = itemView.findViewById(R.id.userDetails);
+            messageCustomerButton = itemView.findViewById(R.id.messageCustomerButton);
+        }
+    }
+}
+
--- app/src/main/java/org/meicode/recycler/SellcustomActivity.java
+++ app/src/main/java/org/meicode/recycler/SellcustomActivity.java
@@ -12,7 +12,7 @@ public class SellcustomActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sellcustom);
 
         // Button for Sellers
         @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button sellersButton = findViewById(R.id.button);
